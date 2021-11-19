1273b2855ea4f0bfbc7b66531daa16e132a8cb06
Annabathina Teja <71269260+teja1705@users.noreply.github.com>
2021-11-19 10:34:37 +0530

    Updated Procurements

--- code/clgx-taxlogix/projects/clgx-taxlogix/src/app/features/agency/view/agency-procurement/agency-procurement.component.ts
+++ code/clgx-taxlogix/projects/clgx-taxlogix/src/app/features/agency/view/agency-procurement/agency-procurement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../../core/core.module';
 
@@ -13,9 +14,11 @@ import { AgencyFeature, agencies } from '../../agency-view.data';
 export class AgencyProcumentComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   agencies: AgencyFeature[] = agencies;
-
   isMobile: boolean = false;
-  constructor( public deviceService:DeviceDetectorService){
+
+  escrow : FormGroup = new FormGroup({})
+  non_escrow : FormGroup = new FormGroup({})
+  constructor( public deviceService:DeviceDetectorService, private fb : FormBuilder){
 
   }
 
@@ -25,9 +28,49 @@ export class AgencyProcumentComponent implements OnInit {
     }else{
       this.isMobile = false;
     }
+    this.escrow = this.fb.group({
+      contactName : [''],
+      contactPhone : [''],
+      contactFax : [''],
+      contactEmail : [''],
+      agencyWebsite : [''],
+      amtAvailableOnWebsite : [''],
+      cost_to_pay_using_copy_of_tb : [''],
+      listing_accepted_for_payment : [''],
+      mail_a_way_only_req : [''],
+      agency_expect_web_tb : [''],
+      postmark_accepted : [''],
+      copy_fee : [''],
+      fee_for_mail_a_way : [''],
+      no_of_parcels_per_check : ['']
+    })
+    this.non_escrow = this.fb.group({
+      collectingAgencyName : [''],
+      contactName : [''],
+      contactPhone : [''],
+      contactFax : [''],
+      contactEmail : [''],
+      agencyWebsite : [''],
+      collectedByAgency : [''],
+      thirdPartyCollections : [''],
+      amtAvailableOnWebsite : [''],
+      mail_a_way_only_req : [''],
+      fee_for_mail_a_way : [''],
+      payToName : [''],
+      payToAddress : [''],
+      payToCity : [''],
+      stateId : [''],
+      zipId : [''],
+      methodOfPaymentRequired_certifiedCheck : [''],
+      methodOfPaymentRequired_wire : ['']
+    })
   }
 
   openLink(link: string) {
     window.open(link, '_blank');
+  }
+  show(form : FormGroup){
+    console.log(form.controls);
+    
   }
 }
