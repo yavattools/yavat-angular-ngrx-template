93f73d110c78f69876e6220c2359d1527489c02d
Yavat <yavattools@gmail.com>
2021-11-22 22:33:12 -0500

    collection dates  data pulled from API

--- code/clgx-taxlogix/projects/clgx-taxlogix/src/app/features/agency/view/agency-collection-practices/agency-collection-practices.component.ts
+++ code/clgx-taxlogix/projects/clgx-taxlogix/src/app/features/agency/view/agency-collection-practices/agency-collection-practices.component.ts
@@ -1,31 +1,14 @@
 import { Component, OnInit, ChangeDetectionStrategy, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator/paginator';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../../core/core.module';
 import {MatTableDataSource} from '@angular/material/table';
 import { AgencyFeature, agencies } from '../../agency-view.data';
-export interface Element {
-  frequency: string;
-  year:  number;
-  installment:  number;
-  base: string;
-  discount: string;
-  penalty: string;
-  lateRelease: string;
-  billRequest: string;
-  
-}
+import { AgencyDataService } from '@app/core/store/agency/agency-data-api.service';
+import { CollectionDates } from '@app/core/store/agency/agency.model';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
-const ELEMENT_DATA: Element[] = [
-  {frequency: 'Quaterly',year:2021,installment:1,base:'2/8/2021',discount:'none',penalty:'3/1/2021',lateRelease:'none',billRequest:'12/31/2021'},
-  {frequency: 'Quaterly',year:2021,installment:2,base:'4/15/2021',discount:'none',penalty:'4/16/2021',lateRelease:'none',billRequest:'1/16/2021'},
-  {frequency: 'Quaterly',year:2021,installment:3,base:'7/15/2021',discount:'none',penalty:'7/16/2021',lateRelease:'none',billRequest:'3/15/2021'},
-  {frequency: 'Quaterly',year:2021,installment:4,base:'9/12/2021',discount:'none',penalty:'9/16/2021',lateRelease:'none',billRequest:'6/12/2021'},
-  {frequency: 'Quaterly',year:2021,installment:1,base:'2/8/2021',discount:'none',penalty:'3/1/2021',lateRelease:'none',billRequest:'12/31/2021'},
-  
- 
- 
-];
+
+
 @Component({
   selector: 'clgx-agency-collection-practices',
   templateUrl: './agency-collection-practices.component.html',
@@ -34,46 +17,66 @@ const ELEMENT_DATA: Element[] = [
 })
 export class AgencyCollectionPracticesComponent implements OnInit {
   displayedColumns :string[]=['frequency', 'year', 'installment', 'base','discount','penalty','lateRelease','billRequest','action'];
-  // dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
-  // @ViewChild(MatPaginator, { static: true })
-  // paginator!: MatPaginator;
-
-  // firstTwoRows: PeriodicElement[] = [];
- dataSource = ELEMENT_DATA;
- 
-
-
- 
-
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   agencies: AgencyFeature[] = agencies;
+  
+  currentDataSource = new MatTableDataSource<CollectionDates>();
+  currentTablelength = 0;
+  currentPageSize = 0;
+
+  @ViewChild('currentPaginator', { read: MatPaginator })
+  currentPaginator!: MatPaginator;
+  
+  historyDataSource = new MatTableDataSource<CollectionDates>();
+  historyTablelength = 0;
+  historyPageSize = 0;
+  historyPageSizeOptions = [5, 10, 25, 100];
+  @ViewChild('historyPaginator', { read: MatPaginator })
+  historyPaginator!: MatPaginator;
+
 
   isMobile: boolean = false;
-  constructor( public deviceService:DeviceDetectorService){
-    // this.firstTwoRows = [
-    //   {frequency: 'Quaterly',year:2021,installment:3,base:'7/15/2021',discount:'none',penalty:'7/16/2021',lateRelease:'none',billRequest:'3/15/2021'},
-    //   {frequency: 'Quaterly',year:2021,installment:4,base:'9/12/2021',discount:'none',penalty:'9/16/2021',lateRelease:'none',billRequest:'6/12/2021'},
-     
-      
-    // ];
-    //     this.dataSource.shift();
-    //     this.dataSource.shift();
+  constructor( public deviceService:DeviceDetectorService, private apiDataService: AgencyDataService){
   }
   
   editDeal(element:Element){
 
   }
-  
- 
+   
   ngOnInit() {
-    //this.dataSource.paginator = this.paginator;
     if(this.deviceService.isMobile()){
       this.isMobile = true;
     }else{
       this.isMobile = false;
     }
+    this.getCurrentCollectionDates();
+    this.getHistoryCollectionDates();
+    this.currentDataSource.paginator = this.currentPaginator;
+    this.currentTablelength = this.currentDataSource.data.length;
+    this.currentPageSize = 4; 
+
+    this.historyDataSource.paginator = this.historyPaginator;
+    this.historyTablelength = this.historyDataSource.data.length;
+    this.historyPageSize = 5; 
   }
+ 
+  ngAfterViewInit(): void {
+    this.currentDataSource.paginator = this.currentPaginator;
+    this.historyDataSource.paginator = this.historyPaginator;
+  }
+
+
+  getCurrentCollectionDates() {
+     this.apiDataService.getCollectionsDates().subscribe(data => {
+        this.currentDataSource.data = data as Array<CollectionDates>;
+     })
+  }
+
+  getHistoryCollectionDates() {
+    this.apiDataService.getCollectionsDates().subscribe(data => {
+       this.historyDataSource.data = data as Array<CollectionDates>;
+    })
+ }
 
   openLink(link: string) {
     window.open(link, '_blank');
